eb7a2202ac4b8c0b0ba48b653b288aaaa4514863
awant-ezzy <awant.ezzy@gmail.com>
2025-11-04 20:10:37 +0700

    Add index.php for Mikrotik Remote Manager

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mikrotik Remote Manager</title>
+</head>
+<body>
+    <h1>Mikrotik Remote Manager</h1>
+
+    <h2>Create Account</h2>
+    <form action="create_account.php" method="post">
+        Username: <input type="text" name="username" required><br>
+        Password: <input type="password" name="password" required><br>
+        Port: <input type="number" name="port" required><br>
+        <button type="submit">Create Account</button>
+    </form>
+
+    <h2>Delete Account</h2>
+    <form action="delete_account.php" method="post">
+        Username: <input type="text" name="username" required><br>
+        <button type="submit">Delete Account</button>
+    </form>
+</body>
+</html>
