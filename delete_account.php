e32cafb39dadc9e7b18a362df7214ea0de24ecbc
awant-ezzy <awant.ezzy@gmail.com>
2025-11-04 20:12:26 +0700

    Implement account deletion for MikroTik users
    This script connects to a MikroTik router and deletes a user account along with associated firewall and NAT rules.

---
+++ delete_account.php
@@ -0,0 +1,39 @@
+<?php
+require_once('RouterosAPI.php');
+
+$mikrotik_ip = '192.168.11.248';
+$mikrotik_username = 'admin';
+$mikrotik_password = 'password'; // Ganti dengan password yang aman
+
+$username = $_POST['username'];
+
+$api = new RouterosAPI();
+$api->debug = false;
+
+if ($api->connect($mikrotik_ip, 8728, $mikrotik_username, $mikrotik_password)) {
+
+    // 1. Remove User
+    $users = $api->comm("/user/print", array("?name" => $username));
+    if (count($users) > 0) {
+        $api->comm("/user/remove", array(".id" => $users[0]['.id']));
+    }
+
+    // 2. Remove Firewall Rule (Filter)
+    $firewall_rules = $api->comm("/ip/firewall/filter/print", array("?comment" => "Allow remote for " . $username));
+    if (count($firewall_rules) > 0) {
+        $api->comm("/ip/firewall/filter/remove", array(".id" => $firewall_rules[0]['.id']));
+    }
+
+    // 3. Remove NAT Rule (dstnat)
+    $nat_rules = $api->comm("/ip/firewall/nat/print", array("?comment" => "NAT for remote " . $username));
+    if (count($nat_rules) > 0) {
+        $api->comm("/ip/firewall/nat/remove", array(".id" => $nat_rules[0]['.id']));
+    }
+
+    $api->disconnect();
+    echo "Account " . $username . " deleted successfully!";
+
+} else {
+    echo "Connection to Mikrotik failed.";
+}
+?>
