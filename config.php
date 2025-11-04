3d9df56475229bddf2bf620362d88d20154f55c1
awant-ezzy <awant.ezzy@gmail.com>
2025-11-04 20:35:05 +0700

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,13 @@ $mikrotik_ip = '192.168.11.248';
 $mikrotik_username = 'nama_user_api';
 $mikrotik_password = 'password_user_api';
 
+// Konfigurasi Database
+$db_host = 'localhost'; // Atau IP server database
+$db_name = 'mikrotik_remote';
+$db_user = 'nama_user_db';
+$db_password = 'password_user_db';
+
 // Konfigurasi Tambahan (opsional)
 $default_group = 'read'; // Group default untuk user baru
-$allowed_src_address = '0.0.0.0/0'; // Alamat IP yang diizinkan (bisa diubah di UI)
+$allowed_src_address = '0.0.0.0/0'; // Alamat IP yang diizinkan
 ?>
