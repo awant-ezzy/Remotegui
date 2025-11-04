e78b207e08bad7583c07aa51cf834e9e5bf28ad4
awant-ezzy <awant.ezzy@gmail.com>
2025-11-04 20:20:00 +0700

    Add configuration for Mikrotik API

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+// Konfigurasi Mikrotik
+$mikrotik_ip = '192.168.11.248';
+$mikrotik_username = 'nama_user_api';
+$mikrotik_password = 'password_user_api';
+
+// Konfigurasi Tambahan (opsional)
+$default_group = 'read'; // Group default untuk user baru
+$allowed_src_address = '0.0.0.0/0'; // Alamat IP yang diizinkan (bisa diubah di UI)
+?>
